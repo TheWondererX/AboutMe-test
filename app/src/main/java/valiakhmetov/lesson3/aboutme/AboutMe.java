56da069b1a2757f776cdfcc841c40808407c431a
TheWondererX <extremeeternity@yandex.ru>
2016-07-24 19:41:45 +0500

    added portrait mode

---
+++ app/src/main/java/valiakhmetov/lesson3/aboutme/AboutMe.java
@@ -0,0 +1,11 @@
+package valiakhmetov.lesson3.aboutme;
+
+/**
+ * Created by Expert on 24.07.2016.
+ */
+public class AboutMe {
+    String surname="";
+    String name="";
+    String lastName="";
+    String email="";
+}
